f4c35e0d3adbb3a757289fd17065f908e06f33fa
kafessizkampanya <133005530+kafessizkampanya@users.noreply.github.com>
2023-12-15 13:35:31 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -1,8 +1,24 @@
 const addresses = [
-    "https://twitter.com/intent/tweet?text=Kafes yumurtası satmaya son verin! @Migros_Turkiye",
-    "https://twitter.com/intent/tweet?text=https://kafessizturkiye.com/migrosdurde sitesini gördünüz mü? Neden henüz kafes yumurtasını terk etmediniz?",
-    "https://twitter.com/intent/tweet?text=Zalim kafeslerden gelen yumurtaları satmayı bırakın", 
-    "https://twitter.com/intent/tweet?text=Kafes yumurtasını reddediyoruz lütfen müşterilerinizi ciddiye alın"];
+    "https://maps.app.goo.gl/fmAAFytrsfZZgZia6",
+    "https://maps.app.goo.gl/ss76aTfvRWdjApMe6",
+    "https://maps.app.goo.gl/7PFeWwcaPVtTggnA6", 
+    "https://maps.app.goo.gl/Rp8TdLSJakzfwaZW8",
+    "https://maps.app.goo.gl/ktTU3tDtrKJPAc419",
+    "https://maps.app.goo.gl/33h4wJh3hPcM9rDA7",
+    "https://maps.app.goo.gl/33h4wJh3hPcM9rDA7",
+    "https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9",
+    "https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9",
+    "https://maps.app.goo.gl/fsDBw4VJLTXTWmK6A",
+    "https://maps.app.goo.gl/c328NPRd9sUfX42Z9",
+    "https://maps.app.goo.gl/MK3nzcRdzu7ukxrC9",
+    "https://maps.app.goo.gl/a72Utejz5Mjnnpe3A",
+    "https://maps.app.goo.gl/RgcQqXKs9g7nERCK6",
+    "https://maps.app.goo.gl/sPdit4i84ZvuUXU49",
+    "https://maps.app.goo.gl/sPdit4i84ZvuUXU49",
+    "https://maps.app.goo.gl/xmHTh2D9RPnMxx518",
+
+
+];
 
 const address_1 = addresses[Math.floor(Math.random() * addresses.length)]
 
