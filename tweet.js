180007fcdc95ecc30936d3d28acfc6848d669236
kafessizkampanya <133005530+kafessizkampanya@users.noreply.github.com>
2024-04-19 13:30:53 +0300

    Update tweet.js

--- tweet.js
+++ tweet.js
@@ -9,19 +9,30 @@ const addresses = [
     "https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9",
     "https://maps.app.goo.gl/QdzEPY2ZsgihyyCc9",
     "https://maps.app.goo.gl/fsDBw4VJLTXTWmK6A",
+    "https://maps.app.goo.gl/fsDBw4VJLTXTWmK6A",
+    "https://maps.app.goo.gl/c328NPRd9sUfX42Z9",
     "https://maps.app.goo.gl/c328NPRd9sUfX42Z9",
     "https://maps.app.goo.gl/MK3nzcRdzu7ukxrC9",
     "https://maps.app.goo.gl/a72Utejz5Mjnnpe3A",
     "https://maps.app.goo.gl/RgcQqXKs9g7nERCK6",
     "https://maps.app.goo.gl/sPdit4i84ZvuUXU49",
     "https://maps.app.goo.gl/sPdit4i84ZvuUXU49",
-    "https://maps.app.goo.gl/xmHTh2D9RPnMxx518",
     "https://maps.app.goo.gl/sPdit4i84ZvuUXU49",
+    "https://maps.app.goo.gl/2WCZ4G3efsqfe5zU6",
+    "https://maps.app.goo.gl/2WCZ4G3efsqfe5zU6",
+    "https://maps.app.goo.gl/2WCZ4G3efsqfe5zU6",
+    "https://maps.app.goo.gl/xmHTh2D9RPnMxx518",
     "https://maps.app.goo.gl/2GUp5UoXxov9dHCL6",
-    "https://maps.app.goo.gl/ycLYSCiPs43mFYZZ8",
     "https://maps.app.goo.gl/FCad2KmeQo6zAgpcA",
+    "https://maps.app.goo.gl/NcCkR7XiErwnExbX6",
+    "https://maps.app.goo.gl/wQbcYX7D82oN8QuB6",
+    "https://maps.app.goo.gl/Nq4ZXnyA5F4GV4Gh9",
+    "https://maps.app.goo.gl/bUmcPzYZFVwu2A8r9",
+    "https://maps.app.goo.gl/bUmcPzYZFVwu2A8r9",
+    "https://maps.app.goo.gl/bUmcPzYZFVwu2A8r9",
+    "https://maps.app.goo.gl/PYYUVc4X6Jzc2E3QA",
+    "https://maps.app.goo.gl/3d9kwgugvfGWL3389"
     
-
 
 ];
 
